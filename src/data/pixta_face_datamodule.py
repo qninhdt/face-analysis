388878fcc5fd5de49c21cb32af16b7e3e33257c7
qninhdt <qndt123@gmail.com>
2024-01-10 01:25:05 +0700

    re

--- src/data/pixta_face_datamodule.py
+++ src/data/pixta_face_datamodule.py
@@ -33,7 +33,7 @@ class PIXTAFaceDataModule(LightningDataModule):
         normalize = Normalize(mean=[0.6596, 0.6235, 0.5875], std=[0.2272, 0.2248, 0.2345])
         
         self.train_transforms = T.Compose([
-            RandomCropWithoutLossingBoxes(),
+            # RandomCropWithoutLossingBoxes(),
             SquarePad(),
             T.RandomRotation(degrees=[-30, 30]),
             T.RandomHorizontalFlip(p=0.5),
